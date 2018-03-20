db5529687200183e808174ccb50b3a8da6458457
Frank Mortensen <fmortens@me.com>
2018-03-20 16:14:43 +0100

    Remove unused logic from UXStore

--- app/stores/UXStore.js
+++ app/stores/UXStore.js
@@ -4,29 +4,5 @@ import {
 } from 'mobx';
 
 export default class UXStore {
-  constructor(props) {
-    this.navigator = props.navigator;
-  }
 
-  @observable.ref
-  navigationState = {
-    index: 0,
-    routes: [
-      {
-        key: 'Login',
-        routeName: 'Login',
-        params: { title: 'Login' }
-      }
-    ]
-  };
-
-  @action
-  dispatch = (action, stackNavState = true) => {
-    const previousNavState = stackNavState ? this.navigationState : null;
-    const newState = (this.navigationState = this.navigator.router.getStateForAction(
-      action,
-      previousNavState
-    ));
-    return true;
-  };
 }
